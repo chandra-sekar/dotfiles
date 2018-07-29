2143e349c5a2ed5aa6d5946a8fb615c84116972a
chandra-sekar <adakadavuley@gmail.com>
2018-07-29 10:23:51 +0530

    Add brew cask script and package control settings

---
+++ brew-cask.sh
@@ -0,0 +1,68 @@
+#Desktop productivity
+brew cask install alfred
+brew cask install caffeine
+brew cask install jumpcut
+brew cask install seil
+brew cask install spectacle
+brew cask install tyke
+brew cask install appcleaner
+brew cask install cheatsheet
+brew cask install freedom
+brew cask install f.lux
+
+#Media
+brew cask install audacity
+brew cask install spotify
+brew cask install vlc
+brew cask install caption
+brew cask install plex-server
+brew cask install transmission
+brew cask install soulseek
+brew cask install handbrake
+brew cask install headset
+
+#Browsers
+brew cask install firefox
+brew cask install google-chrome
+
+#Development
+brew cask install postman
+brew cask install sequel-pro
+brew cask install mamp
+brew cask install robomongo
+brew cask install github-desktop
+brew cask install sublime-text
+brew cask install imageoptim
+
+#Conference calls & Messaging
+brew cask install skype
+brew cask install ringcentral
+brew cask install zoom.us
+brew cask install slack
+brew cask install telegram
+brew cask install colloquy
+
+#Utilities
+brew cask install licecap
+brew cask install filezilla
+brew cask install virtualbox
+brew cask install the-unarchiver
+brew cask install calibre
+brew cask install onyx
+brew cask install fontstand
+brew cask install xscope
+brew cask install helium
+brew cask install openemu
+brew cask install numi
+brew cask install kindle
+
+#Quick look  plugings
+brew cask install qlcolorcode
+brew cask install qlstephen
+brew cask install qlmarkdown
+brew cask install quicklook-json
+brew cask install qlprettypatch
+brew cask install quicklook-csv
+brew cask install betterzipql
+brew cask install webpquicklook
+brew cask install suspicious-package
