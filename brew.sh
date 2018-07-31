27e7d46708539564df4266ff9fef1c43d405d49a
chandra-sekar <adakadavuley@gmail.com>
2018-07-31 12:44:10 +0530

    Added homebrew installation script

--- brew.sh
+++ brew.sh
@@ -2,6 +2,9 @@
 
 # Install command-line tools using Homebrew.
 
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # Make sure we’re using the latest Homebrew.
 brew update
 
