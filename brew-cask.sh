70aa589b2f80e7754f20c7703487dc2127098001
chandra-sekar <adakadavuley@gmail.com>
2018-07-31 12:40:51 +0530

    Removed unwanted quicklook plugins

--- brew-cask.sh
+++ brew-cask.sh
@@ -56,11 +56,8 @@ brew cask install kindle
 
 #Quick look  plugings
 brew cask install qlcolorcode
-brew cask install qlstephen
 brew cask install qlmarkdown
 brew cask install quicklook-json
 brew cask install qlprettypatch
 brew cask install quicklook-csv
-brew cask install betterzipql
-brew cask install webpquicklook
 brew cask install suspicious-package
